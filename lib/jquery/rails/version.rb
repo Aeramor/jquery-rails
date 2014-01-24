584b5ec3878b0016cc3baff535086a604e15fbdf
Randy Bares <aeramor@gmail.com>
2014-01-24 13:27:15 -0800

    Updated jquery_ujs version to current commit.

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -2,6 +2,6 @@ module Jquery
   module Rails
     VERSION = "3.1.0"
     JQUERY_VERSION = "2.1.0"
-    JQUERY_UJS_VERSION = "e9e8b8fd3abb1571781bca7640e5c0c4d9cea2e3"
+    JQUERY_UJS_VERSION = "c7dcc344eefcd9a646fe570f00fec73078ecb6c4"
   end
 end
